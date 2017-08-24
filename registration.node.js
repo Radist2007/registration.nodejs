aeb8416100eb22cea50c02eaeb7b9b264c13051b
Alex <izofatov2007@gmail.com>
2017-08-24 22:54:01 +0300

    add expres&handlbars

--- registration.node.js
+++ registration.node.js
@@ -1,12 +1,52 @@
-var express = require('express');
+var express = require('express');//Подключаем експрес
 
-var app = express();
+var app = express();//
 
 //handlebars view engine
 var handlebars = require('express-handlebars').create({ defaultLayout: 'main'});
 
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars.engine);//Подключаем движок хендлбар
+app.set('view engine', 'handlebars');//Устанавливаем хендлбарс
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);//Устанавливаем порт
 
+app.use(express.static(__dirname + '/public'));
+app.use(require('body-parser')());
+
+app.get('/', function(req, res){
+	res.render('home');
+	console.log('/ -->/home'); 
+});
+
+app.get('/home', function(req, res){
+	res.render('home');
+	console.log('/home');
+});
+
+app.get('/sendMail', function(req, res){
+	res.render('sendMail');
+	console.log('/sendMail');
+});
+
+app.get('/about', function(req, res){
+	res.render('about');
+	console.log('/about');
+});
+
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+	res.status(404);
+	res.render('404');
+});
+
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log( 'Express started on http://localhost:' + 
+    app.get('port') + '; press Ctrl-C to terminate.' );
+});
